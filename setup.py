bf753e1d0fdd414194e98fabb0f8bd6fa6103ba1
jujusb <jsantilariob@gmail.com>
2020-08-19 13:55:21 +0200

    Add dashboard for interactions with tabs + addition of a file selector + add of a setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='XAPI Serious Game Processor',
+      version='0.5',
+      description='Processor of traces following the Experience API for Serious Games Profile (xAPI-SG)',
+      author='eUCM Group, Julio SANTILARIO BERTHILLIER',
+      author_email='',
+      install_requires=['ipympl', 'json', 'numpy','datetime', 'copy', 'math', 'matplotlib','collections', 'pandas', 'ipywidgets', 'IPython', 'pprint'],
+     )
