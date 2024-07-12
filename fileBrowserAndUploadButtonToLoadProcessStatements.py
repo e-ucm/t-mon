05ccea2b86614dbf83c4d9b6a8e2f0125cc3e14e
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-07-12 14:12:04 +0200

    adding xapi table

--- fileBrowserAndUploadButtonToLoadProcessStatements.py
+++ fileBrowserAndUploadButtonToLoadProcessStatements.py
@@ -45,7 +45,7 @@ def log(target, o_str):
 #    an object o where with o.output: print() is valid
 #
 # Callback to update progress periodically
-def load_from_string(str, progress, info_output, err_output, players_info, timeformats):
+def load_from_string(str, progress, players_info, xapiData, timeformats, info_output, err_output):
     total=0
     count=0
     try:
@@ -54,10 +54,13 @@ def load_from_string(str, progress, info_output, err_output, players_info, timef
             total=len(str.splitlines())
             log(info_output, f"... 1st line is valid JSON; interpreting as one-statement-per-line ({total} statement(s))")
             # 1st line is well-formed json, and not json list of statements; assume 1-statement-per-line
-            for s in str.splitlines():
+            statements=str.splitlines()
+            for statement in statements:
+                s=json.loads(statement)
                 progress.value=count/total
                 count+=1
-                ProcessxAPISGStatement.process_xapisg_statement(json.loads(s), players_info, timeformats)
+                ProcessxAPISGStatement.process_xapisg_statement(s, players_info, timeformats)
+                xapiData.append(s)
         else:
             log(info_output, "... interpreting as statement-array")
             # attempt to process all of it as a single JSON document
@@ -68,6 +71,7 @@ def load_from_string(str, progress, info_output, err_output, players_info, timef
                 progress.value=count/total
                 count+=1
                 ProcessxAPISGStatement.process_xapisg_statement(s, players_info, timeformats)
+            xapiData.extend(statements)
     except Exception as e:
         log(err_output, 
             f"ERROR loading at line/statement {count}/{total}: {e}\n"
@@ -79,19 +83,19 @@ def load_from_string(str, progress, info_output, err_output, players_info, timef
 
 timeformats=['%Y-%m-%dT%H:%M:%SZ','%Y-%m-%dT%H:%M:%S.%fZ']
 
-def load_players_info_from_file(file, players_info, out, err):
+def load_players_info_from_file(file, players_info,xapiData, out, err):
     with open(file, encoding="utf-8") as f:
         str = f.read()
         progress = Progress(0)
-        load_from_string(str, progress, out, err, players_info, timeformats)
+        load_from_string(str, progress, players_info, xapiData, timeformats, out, err)
         print(f"Info log ({len(out)} lines):\n", '\n'.join(out))
         if len(err) > 0:
             print(f"ERRORS FOUND ({len(err)} lines):\n", '\n'.join(err))
             sys.exit(-1)
     return players_info
 
-def load_players_info_from_content(filecontent, filename, players_info, out, err):
+def load_players_info_from_content(filecontent, filename, players_info, xapiData, out, err):
     content_type, content_string = filecontent.split(',')
     decoded = base64.b64decode(content_string)
     progress = Progress(0)
-    load_from_string(decoded.decode('utf-8'), progress, out, err, players_info, timeformats)
+    load_from_string(decoded.decode('utf-8'), progress, players_info, xapiData, timeformats, out, err)
