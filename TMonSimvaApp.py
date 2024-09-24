5820d5c4c47564872eacadd2b3aa4c2bcad44f01
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-09-24 15:11:10 +0200

    remove unecesary code

--- TMonSimvaApp.py
+++ TMonSimvaApp.py
@@ -4,13 +4,6 @@ from TMonWidgets import SimvaBrowserWidget, TMonWidget
 # Initialize Dash app with Flask server
 app = Dash(__name__, server=SimvaBrowserWidget.flask.flaskServer)
 
-# If you set host or port differently
-print(app.server.config)
-httpSecure= "https" if app.server.config.get('SECURE', False) == "True" else "http"
-debug=app.server.config.get('DEBUG', "False")
-host = app.server.config.get('HOST', '0.0.0.0')
-port = app.server.config.get('PORT', 8050)
-
 # Layout of the dashboard
 app.layout=html.Div(
     [
@@ -24,6 +17,5 @@ app.layout=html.Div(
 
 if __name__ == '__main__':
     # Construct the URL
-    url = f'{httpSecure}://{host}:{port}'
-    print(f'The app is running at {url} (http://0.0.0.0:8050/) - {debug}')
+    print(f'The app is running at http://0.0.0.0:8050/')
     app.run(debug=True, host="0.0.0.0", port="8050")
