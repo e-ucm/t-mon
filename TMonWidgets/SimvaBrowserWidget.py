39feacb771b483880a7e52337e018de7f671b7ba
Julio SANTILARIO BERTHILIER <62715763+jujusb@users.noreply.github.com>
2024-10-07 15:45:57 +0200

    combined with upload button

--- TMonWidgets/SimvaBrowserWidget.py
+++ TMonWidgets/SimvaBrowserWidget.py
@@ -6,23 +6,33 @@ from dash.exceptions import PreventUpdate
 import json
 import TMonWidgets
 #Import LoadProcessStatements.py
-from LoadProcessStatements import load_from_string
+from LoadProcessStatements import load_from_string, load_players_info_from_uploaded_content
 # Import SimvaBrowser class from SimvaBrowser.py
 from SimvaBrowser.SimvaBrowser import SimvaBrowser
 # Import KeycloakClient class containing a Flask OIDC server from KeycloakClient.py
 from SimvaBrowser.KeycloakClient import KeycloakClient
+from datetime import datetime
 flask=KeycloakClient(homepage=False)
 
 # Dash callback to handle login button click
 @callback(
-    Output('login-logout-button', 'children'),
+    [Output('login-logout-button', 'children'),
+     Output('upload-data','style')],
     [Input('main-login', 'children')]
 )
 def login_logout_button_displayed(main):
     if flask.oidc.user_loggedin:
-        return "Logout"
+        return "Logout", {'display': 'None'}
     else:
-        return "Login"    
+        return "Login", {'display': 'block',
+                         'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'}  
 
 # Dash callback to handle login button click
 @callback(
@@ -32,6 +42,7 @@ def login_logout_button_displayed(main):
 def login_logout_button_click(n_clicks):
     if n_clicks > 0:
         if flask.oidc.user_loggedin:
+            browser=None
             return dcc.Location(pathname='/logoutkeycloak', id='login-logout-link')
         else:
             return dcc.Location(pathname='/login', id='login-logout-link')
@@ -118,7 +129,17 @@ def init_storage(main, pathname):
         ])
         return appLayout
     else:
-        return html.H4("Connect to your SIMVA account to access to your data.")
+        return html.Div([
+                html.H4("Connect to your SIMVA account to access to your data."),
+                html.H3(id='current-path', style={'display': 'none'}),
+                html.H4(id='current-study', style={'display': 'none'}),
+                html.H4(id='current-test', style={'display': 'none'}),
+                html.H4(id='current-activity', style={'display': 'none'}),
+                html.Button('..', id='parent-directory', n_clicks=0, style={'display': 'none'}),
+                html.Div(id='folders-div', children=[], style={'display': 'none'}),
+                html.Div(id='files-div', children=[], style={'display': 'none'}),
+                html.Button('Run Analyse', id='run-analyse', n_clicks=0, style={'display': 'none'}),
+            ])
 
 @callback(
     [Output('current-path', 'children'),
@@ -135,91 +156,119 @@ def init_storage(main, pathname):
     [Input('parent-directory', 'n_clicks'),
      Input({'type': 'folder-button', 'index': dash.dependencies.ALL}, 'n_clicks'),
      Input({'type': 'file-button', 'index': dash.dependencies.ALL}, 'n_clicks'),
-     Input('run-analyse', 'n_clicks')],
+     Input('run-analyse', 'n_clicks'),
+     Input('upload-data', 'contents')],
     [State('current-path', 'children'),
-     State('url', 'pathname')]
+     State('url', 'pathname'),
+     State('upload-data', 'filename'),
+    State('upload-data', 'last_modified')]
 )
-def update_browser(n_clicks_parent, folder_n_clicks, file_n_clicks, n_clicks_run_analyse, current_path, statepathname):
-    ctx = dash.callback_context
-    res=f"{current_path} - triggered : {ctx.triggered} - Files : {file_n_clicks} - Folders : {folder_n_clicks} - n_clicks_parent : {n_clicks_parent} - n-clicks-run-analyse : {n_clicks_run_analyse}"
-    print(res)
-    if not ctx.triggered:
-        raise PreventUpdate
-    triggered_prop_id = ctx.triggered[0]['prop_id']
-    print(f'PropId : {triggered_prop_id} - State : {statepathname} - {browser.current_path} - {browser.base_path}')
-    # Find the position of "/dashboard/"
-    index = statepathname.find("/dashboard")
-    # Slice the string up to the index if "/dashboard/" is found
-    if index != -1:
-        newstatepathname = statepathname[:index]
-        dashboard_url = statepathname[index:]
-        run_dashboard=True
+def update_browser(n_clicks_parent, folder_n_clicks, file_n_clicks, n_clicks_run_analyse, list_of_contents, current_path, statepathname, list_of_names, list_of_dates):
+    try:
+        browser
+    except NameError:
+        print("Well, it WASN'T defined after all!")
+        if list_of_names:
+            print(f"List_of_names : {list_of_names} - list_of_dates : {list_of_dates}")
+            div_list = []
+            TMonWidgets.xapiData = []
+            nbError=0
+            style={'display': 'block'}
+            for c, n, d in zip(list_of_contents, list_of_names, list_of_dates):
+                out, err = [], []
+                div_list.append(html.H5(n))
+                div_list.append(html.H6(datetime.fromtimestamp(d)))
+                load_players_info_from_uploaded_content(c, n, TMonWidgets.xapiData, out, err)
+                div_list.append(html.Div(out))
+                div_list.append(html.Div(err))
+                if len(err) > 0 : 
+                    nbError+=1
+                div_list.append(html.Hr())
+            if nbError == len(list_of_names):
+                style={'display': 'none'}
+            return html.Div(),html.Div(),html.Div(),html.Div(),html.Div(),html.Div(),{'display': 'none'}, div_list, style, f"/dashboard/tab=home_tab"
     else:
-        newstatepathname = statepathname
-        dashboard_url = ""
-        run_dashboard=False
-    print(f"run_dashboard : {run_dashboard}")
-    # Remove the .n_clicks suffix
-    if 'parent-directory' in triggered_prop_id and int(n_clicks_parent)>0:
-        print(f"Current Path : {browser.current_path} - State : {newstatepathname} ")
-        if len(browser.current_path) > len(browser.base_path):
-            if browser._isdir(browser.current_path):
-                browser.current_path = browser.current_path.rpartition(browser.delimiter)[0]
-            browser.current_path = browser.current_path.rpartition(browser.delimiter)[0] + browser.delimiter
+        print("Sure, it was defined.")
+        ctx = dash.callback_context
+        res=f"{current_path} - triggered : {ctx.triggered} - Files : {file_n_clicks} - Folders : {folder_n_clicks} - n_clicks_parent : {n_clicks_parent} - n-clicks-run-analyse : {n_clicks_run_analyse}"
+        print(res)
+        if not ctx.triggered:
+            raise PreventUpdate
+        triggered_prop_id = ctx.triggered[0]['prop_id']
+        print(f'PropId : {triggered_prop_id} - State : {statepathname} - {browser.current_path} - {browser.base_path}')
+        # Find the position of "/dashboard/"
+        index = statepathname.find("/dashboard")
+        # Slice the string up to the index if "/dashboard/" is found
+        if index != -1:
+            newstatepathname = statepathname[:index]
+            dashboard_url = statepathname[index:]
+            run_dashboard=True
         else:
-            browser.current_path = browser.base_path
-        pathname=browser.current_path.replace(browser.base_path, "/")
-        print(f"{browser.current_path} - New Path : {pathname}")
-    elif ('run-analyse' in triggered_prop_id and int(n_clicks_run_analyse)>0) or run_dashboard:
-        run_analyse_style={'display': 'none'}
-        folder_buttons=[]
-        file_buttons=[]
-        TMonWidgets.xapiData=[]
-        div_list= []
-        out=[]
-        err=[]
-        current_file_path, content_string = browser.get_file_content_from_url()
-        load_from_string(
-            content_string, TMonWidgets.xapiData, out, err
-        )
-        div_list.append(html.Div([
-                html.Div(out),
-                html.Div(err),
-                html.Hr(),
-            ]))
-        pathname=newstatepathname
-        if run_dashboard:
-            dashboardpath=f"{dashboard_url}"
+            newstatepathname = statepathname
+            dashboard_url = ""
+            run_dashboard=False
+        print(f"run_dashboard : {run_dashboard}")
+        # Remove the .n_clicks suffix
+        if 'parent-directory' in triggered_prop_id and int(n_clicks_parent)>0:
+            print(f"Current Path : {browser.current_path} - State : {newstatepathname} ")
+            if len(browser.current_path) > len(browser.base_path):
+                if browser._isdir(browser.current_path):
+                    browser.current_path = browser.current_path.rpartition(browser.delimiter)[0]
+                browser.current_path = browser.current_path.rpartition(browser.delimiter)[0] + browser.delimiter
+            else:
+                browser.current_path = browser.base_path
+            pathname=browser.current_path.replace(browser.base_path, "/")
+            print(f"{browser.current_path} - New Path : {pathname}")
+        elif ('run-analyse' in triggered_prop_id and int(n_clicks_run_analyse)>0) or run_dashboard:
+            run_analyse_style={'display': 'none'}
+            folder_buttons=[]
+            file_buttons=[]
+            TMonWidgets.xapiData=[]
+            div_list= []
+            out=[]
+            err=[]
+            current_file_path, content_string = browser.get_file_content_from_url()
+            load_from_string(
+                content_string, TMonWidgets.xapiData, out, err
+            )
+            div_list.append(html.Div([
+                    html.Div(out),
+                    html.Div(err),
+                    html.Hr(),
+                ]))
+            pathname=newstatepathname
+            if run_dashboard:
+                dashboardpath=f"{dashboard_url}"
+            else:
+                dashboardpath=f"/dashboard/tab=home_tab"
+            print(f"Pathname : {pathname} - State : {newstatepathname} - dashboardurl : {dashboard_url}")
+            actual_study=browser.actual_study.get("name") if browser.actual_study is not None else "Select a study"
+            actual_test=browser.actual_test.get("name") if browser.actual_test is not None else "Select a test"
+            actual_activity=browser.actual_activity.get("name") if browser.actual_activity is not None else "Select an activity"
+            if(len(err) > 0):
+                return browser.current_path,actual_study, actual_test, actual_activity, folder_buttons, file_buttons, run_analyse_style, html.Div(div_list), {'display': 'none'}, f"{pathname}{dashboardpath}" 
+            else:
+                return browser.current_path,actual_study, actual_test, actual_activity, folder_buttons, file_buttons, run_analyse_style, html.Div(div_list), {'display': 'block'}, f"{pathname}{dashboardpath}"
+        elif "-button"in triggered_prop_id:
+            cleaned_prop_id = triggered_prop_id.replace(".n_clicks", "")
+            button_id = json.loads(cleaned_prop_id)
+            if button_id['type'] == 'folder-button':
+                browser.current_path = browser.current_path + button_id['index']
+            elif button_id['type'] == 'file-button':
+                browser.current_path = browser.current_path + button_id["index"]
+            pathname=browser.current_path.replace(browser.base_path, "/")
         else:
-            dashboardpath=f"/dashboard/tab=home_tab"
-        print(f"Pathname : {pathname} - State : {newstatepathname} - dashboardurl : {dashboard_url}")
+            print("Nothing to do ! Prevent update")
+            raise PreventUpdate
+        browser._update_files()
+        folder_buttons = [html.Button(f"{f.get('name')} ({f.get('id')})", id={'type': 'folder-button', 'index': f.get("id")}, n_clicks=0) for f in browser.dirs]
+        file_buttons = [html.Button(f, id={'type': 'file-button', 'index': f}, n_clicks=0, style={'backgroundColor': 'green'}) for f in browser.files if f.endswith(browser.accept)]
+        run_analyse_style = {'display': 'none'} if browser._isdir(browser.current_path) else {'display': 'block'}
+        print("Pathname:", pathname)
         actual_study=browser.actual_study.get("name") if browser.actual_study is not None else "Select a study"
         actual_test=browser.actual_test.get("name") if browser.actual_test is not None else "Select a test"
         actual_activity=browser.actual_activity.get("name") if browser.actual_activity is not None else "Select an activity"
-        if(len(err) > 0):
-            return browser.current_path,actual_study, actual_test, actual_activity, folder_buttons, file_buttons, run_analyse_style, html.Div(div_list), {'display': 'none'}, f"{pathname}{dashboardpath}" 
-        else:
-            return browser.current_path,actual_study, actual_test, actual_activity, folder_buttons, file_buttons, run_analyse_style, html.Div(div_list), {'display': 'block'}, f"{pathname}{dashboardpath}"
-    elif "-button"in triggered_prop_id:
-        cleaned_prop_id = triggered_prop_id.replace(".n_clicks", "")
-        button_id = json.loads(cleaned_prop_id)
-        if button_id['type'] == 'folder-button':
-            browser.current_path = browser.current_path + button_id['index']
-        elif button_id['type'] == 'file-button':
-            browser.current_path = browser.current_path + button_id["index"]
-        pathname=browser.current_path.replace(browser.base_path, "/")
-    else:
-        print("Nothing to do ! Prevent update")
-        raise PreventUpdate
-    browser._update_files()
-    folder_buttons = [html.Button(f"{f.get('name')} ({f.get('id')})", id={'type': 'folder-button', 'index': f.get("id")}, n_clicks=0) for f in browser.dirs]
-    file_buttons = [html.Button(f, id={'type': 'file-button', 'index': f}, n_clicks=0, style={'backgroundColor': 'green'}) for f in browser.files if f.endswith(browser.accept)]
-    run_analyse_style = {'display': 'none'} if browser._isdir(browser.current_path) else {'display': 'block'}
-    print("Pathname:", pathname)
-    actual_study=browser.actual_study.get("name") if browser.actual_study is not None else "Select a study"
-    actual_test=browser.actual_test.get("name") if browser.actual_test is not None else "Select a test"
-    actual_activity=browser.actual_activity.get("name") if browser.actual_activity is not None else "Select an activity"
-    return browser.current_path, actual_study,actual_test, actual_activity, folder_buttons, file_buttons, run_analyse_style, html.H1(""), {'display': 'none'}, pathname
+        return browser.current_path, actual_study,actual_test, actual_activity, folder_buttons, file_buttons, run_analyse_style, html.H1(""), {'display': 'none'}, pathname
 
 simvaBrowserBody = html.Div(
     [
@@ -234,7 +283,26 @@ simvaBrowserBody = html.Div(
                 html.Div(id='files-div', children=[]),
                 html.Button('Run Analyse', id='run-analyse', n_clicks=0, style={'display': 'none'}),
             ]
-        ), 
+        ),
+        dcc.Upload(
+            id='upload-data',
+            children=html.Div([
+                'Drag and Drop or ',
+                html.A('Select Files')
+            ]),
+            style={
+                'width': '100%',
+                'height': '60px',
+                'lineHeight': '60px',
+                'borderWidth': '1px',
+                'borderStyle': 'dashed',
+                'borderRadius': '5px',
+                'textAlign': 'center',
+                'margin': '10px'
+            },
+            # Allow multiple files to be uploaded
+            multiple=True
+        ),
         html.Div(id='debug-browser', children=[]),
         html.Div(id='content',children=[])
     ]
